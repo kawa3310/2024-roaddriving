48019e355f6756d0e494e8a7f0d3b8ead83db513
kawa <>
2024-08-19 11:04:25 +0800

    git_建立專案

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import './assets/all.scss'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import { BootstrapIconsPlugin } from 'bootstrap-icons-vue';
+
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(BootstrapIconsPlugin);
+
+app.mount('#app')
