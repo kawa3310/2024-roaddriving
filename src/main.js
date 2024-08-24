b896b50173f4cd040c16cf74d498f19aa96a15be
kawa <>
2024-08-24 10:46:55 +0800

    新增分號

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
-import './assets/all.scss'
+import './assets/all.scss';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
 import { BootstrapIconsPlugin } from 'bootstrap-icons-vue';
 
 
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import router from './router';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
 app.use(BootstrapIconsPlugin);
 
 app.mount('#app')
