b41072987099e62126a3bdaf70320038fae1b51c
kawa <>
2024-09-10 20:22:46 +0800

    feat：拆分home頁面footer

--- src/views/FrontView.vue
+++ src/views/FrontView.vue
@@ -1,7 +1,7 @@
 <template>
   <NavbarComponents/>
     <RouterView/>
-  <footer class="mt-7 mt-lg-8">
+  <footer class="mt-7 mb-3 mb-lg-4 mt-lg-8">
     <div class="container">
       <div class="d-flex justify-content-start justify-content-lg-between
       align-items-center d-grid gap-3 mb-4">
@@ -73,13 +73,4 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .footer-logo{
-    width: 48px;
-  }
-  .community-icon{
-    max-width: 40px;
-  }
-  .statement{
-    font-size: 14px;
-  }
 </style>
--- src/views/FrontView/HomeView.vue
+++ src/views/FrontView/HomeView.vue
@@ -85,7 +85,7 @@
       <HomeSwiper class="d-lg-block d-md-block d-none"></HomeSwiper>
     </div>
   </section>
-  <div class="banner2 mt-lg-9 mt-7">
+  <div class="banner2 mt-7">
     <div class="container">
       <div class="pt-9 d-lg-block d-md-block d-none">
         <p class="fs-1 fw-bold text-white">不用怕!</p>
@@ -99,7 +99,7 @@
       </div>
     </div>
   </div>
-  <main class="main my-7 mt-lg-9">
+  <main class="main pt-lg-9 py-7">
     <div class="container">
       <div class="text-center text-primary pb-7 pb-lg-9">
         <p class="main-title fs-1 fw-bold">
@@ -143,7 +143,7 @@
           </li>
           <li class="text-center"><h2>抓不準變化車道的時機</h2></li>
         </ul>
-        <div class="button d-flex justify-content-center justify-content-lg-end mt-3 mb-5 my-lg-8">
+        <div class="button d-flex justify-content-center justify-content-lg-end mt-3 ">
           <button type="button" class="btn btn-primary d-flex justify-content-center
           align-items-center text-white">
             還想看更多<i class="bi bi-arrow-right-circle ps-2"></i>
@@ -152,7 +152,7 @@
       </div>
     </div>
   </main>
-  <section class="tips mt-9">
+  <section class="tips my-lg-9 mt-7">
     <div class="container">
       <div class="text-center text-primary pb-8">
         <p class="main-title fs-1 fw-bold">課程特色</p>
@@ -252,10 +252,10 @@
       </div>
     </div>
   </section>
-  <section class="teachers mt-7 mb-7 mt-lg-9">
-    <div class="container pt-8">
+  <section class="teachers py-lg-9 pb-7">
+    <div class="container">
       <div class="teachers-sm d-lg-none d-md-none">
-        <div class="text-center text-primary pb-4">
+        <div class="text-center text-primary pb-4 pb-lg-8">
           <p class="main-title fs-1 fw-bold">
             北中南最優秀的師資
           </p>
@@ -428,7 +428,7 @@
           </div>
         </div>
         <div>
-          <div class="area pt-8">
+          <div class="area pt-8 text-end">
             <button type="button" class="btn btn-primary text-white fs-4 fw-bold">中部</button>
           </div>
           <div class="mt-5">
@@ -591,6 +591,65 @@
       </div>
     </div>
   </section>
+  <footer class="mt-7 mb-3 mb-lg-4 mt-lg-8">
+    <div class="container">
+      <div class="d-flex justify-content-start justify-content-lg-between
+      align-items-center d-grid gap-3 mb-4">
+        <div class="d-flex justify-content-start align-items-center">
+          <img src="https://storage.googleapis.com/vue-course-api.appspot.com/reirei/1725180420751.png?GoogleAccessId=firebase-adminsdk-zzty7%40vue-course-api.iam.gserviceaccount.com&Expires=1742169600&Signature=GklhkUuD6Ur0vIqY3rnQGa2woUnl2YWwQrXQ3CUCcK0tRg5lPWMjfs9VEUYJZqM9K3TDbAbxNbhknJ7zMaGzz2M1S95XsS96SFuURt1vw3845%2FDqnoUjEq6iiLsg%2B72inxrCArwo%2Bmp5ip1riXIEShkQ8r2QpULNM51xQwFLbBNw3B%2BLJmG89FG2T2Axr58x1L7pvpoNV8rjn%2FedHU%2BigKM8SQduZAL7D%2Fatp9gh1y%2FgF4T%2FCFiCtyxshmZppWf62zbJgeVMIk7zU4%2B3nEhplM03JgSwMvdulqBcukvdmNx%2Fv5wYGiL5hk5WLr9OicksGsVN9dI9l0wodblATub%2B0A%3D%3D" class="footer-logo" alt="footer-logo">
+          <h2 class="text-primary ps-lg-3 ps-2">南南道路駕訓</h2>
+        </div>
+        <ul class="community d-lg-flex
+        d-none justify-content-center align-items-center d-grid gap-3 pt-3">
+          <li>
+            <a href="#">
+              <img src="https://storage.googleapis.com/vue-course-api.appspot.com/reirei/1725193180155.png?GoogleAccessId=firebase-adminsdk-zzty7%40vue-course-api.iam.gserviceaccount.com&Expires=1742169600&Signature=XMYtEjBF4Y7Z1LArnwmSBlIsW7qvsz1%2BLeMreMI5I%2BvwtXgjrfporGbIqStTFurDyiguSgAMOKdCTODWYwQDEw5cdP%2B6DcZ%2BSLgDeFOK%2Fi%2Fz422ODfKyE4%2BDCJyczoAxkMCucKRMI525qQhuMw6GYkfa7Q33t%2BdTBGLMsCdmY%2B66P8hmSeq9F%2Fg5AxH08nyuU0Kc3Hcv2Y2AHZIKwcAXNL3%2Ft6GAGEMCqKL%2FxdOvs1ou4HIxbBU1CnxZF9FnrlzVoNPDQGcQN%2Bo52B5q4cgBI53dpU6JzBQYp0td%2BguC%2F1%2BlCuZHdqLjcE%2B43wIuQbeV8778QZ4Fw65k0330YJSLxQ%3D%3D" alt="facebook-icon" class="community-icon">
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src="https://storage.googleapis.com/vue-course-api.appspot.com/reirei/1725193204073.png?GoogleAccessId=firebase-adminsdk-zzty7%40vue-course-api.iam.gserviceaccount.com&Expires=1742169600&Signature=flSD9%2Fw1I255ysDeYznpTlqUsiZemABYGJJ9GupAFfqThKP%2FWKXPh%2F01NX6ad4ILQUrWQAI3k46VgSwWLVJJ%2Bd2OMwW0eoun4z7YnsCK7f78rA1ct14o%2Bpg9AOKWhENEtd1I960wb0YbK5HxA%2F%2FJnarUEidH6phJ4%2Bkdd4G%2BSm7iqLjFbi2pUCzYLcoZ%2Buzt5H%2B1MyMPgQ8IosbyASHpcC94pyUj5%2BvoK%2FtugYFZUkZYx3QCIjNBjbyeI%2B1trF3aKl3Q8Po2%2FBh%2BGbbVAKTsFQoA%2FB%2BscvosQ8aiAu2k2oYY463HEX807K8%2FKfgiQmpk%2BJkrTLbklIJpcMFFqftuZg%3D%3D" alt="instagram-icon" class="community-icon">
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src="https://storage.googleapis.com/vue-course-api.appspot.com/reirei/1725193225623.png?GoogleAccessId=firebase-adminsdk-zzty7%40vue-course-api.iam.gserviceaccount.com&Expires=1742169600&Signature=SaarVV82Nzp41uF0gTADC2zwIDTV2iV4882wZIx3StPDApMoT56pLBlp%2BOEk%2FSCUmAgVuZNvf3k9ZsdmAnu1Z%2Bs4CMrMGl3ORQfpacxSa73b0x6s3Ihn3grw4uV0PF9KzmWubni0OW8aQE%2F7nEkj0e5KcqqRu%2BHtMGKUTUzChSW1caMP93wxBVyufOyfrvINMnqIFBphXvlkCGHuOLn%2FXqo2for9mGuAOXOzQsBqIBAKX3wmCku5B8p2mobvNvYee2Qvxj4plubVLFSnEm9B4HvAMfT%2BZEf08osTvUmYtSFOVIZ6TBJDzCUfoAu4SWjF9112tLkL2B%2FPGZXaspVqPw%3D%3D" alt="twitter-icon" class="community-icon">
+            </a>
+          </li>
+        </ul>
+      </div>
+      <ul class="contact d-grid gap-3 gap-lg-4 pb-3">
+        <li class="d-flex align-items-center d-grid gap-2">
+          <i class="bi bi-telephone"></i>
+          <a href="tel:+07-4569477">07-4569477</a>
+        </li>
+        <li class="d-flex align-items-center d-grid gap-2">
+          <i class="bi bi-geo-alt"></i>
+          <a href="mailto:roaddining@gmail.com">roaddining@gmail.com</a>
+        </li>
+      </ul>
+      <ul class="community d-lg-none d-flex
+      justify-content-center align-items-center d-grid gap-3 pt-3">
+        <li>
+          <a href="#">
+            <img src="https://storage.googleapis.com/vue-course-api.appspot.com/reirei/1725193180155.png?GoogleAccessId=firebase-adminsdk-zzty7%40vue-course-api.iam.gserviceaccount.com&Expires=1742169600&Signature=XMYtEjBF4Y7Z1LArnwmSBlIsW7qvsz1%2BLeMreMI5I%2BvwtXgjrfporGbIqStTFurDyiguSgAMOKdCTODWYwQDEw5cdP%2B6DcZ%2BSLgDeFOK%2Fi%2Fz422ODfKyE4%2BDCJyczoAxkMCucKRMI525qQhuMw6GYkfa7Q33t%2BdTBGLMsCdmY%2B66P8hmSeq9F%2Fg5AxH08nyuU0Kc3Hcv2Y2AHZIKwcAXNL3%2Ft6GAGEMCqKL%2FxdOvs1ou4HIxbBU1CnxZF9FnrlzVoNPDQGcQN%2Bo52B5q4cgBI53dpU6JzBQYp0td%2BguC%2F1%2BlCuZHdqLjcE%2B43wIuQbeV8778QZ4Fw65k0330YJSLxQ%3D%3D" alt="facebook-icon" class="community-icon">
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img src="https://storage.googleapis.com/vue-course-api.appspot.com/reirei/1725193204073.png?GoogleAccessId=firebase-adminsdk-zzty7%40vue-course-api.iam.gserviceaccount.com&Expires=1742169600&Signature=flSD9%2Fw1I255ysDeYznpTlqUsiZemABYGJJ9GupAFfqThKP%2FWKXPh%2F01NX6ad4ILQUrWQAI3k46VgSwWLVJJ%2Bd2OMwW0eoun4z7YnsCK7f78rA1ct14o%2Bpg9AOKWhENEtd1I960wb0YbK5HxA%2F%2FJnarUEidH6phJ4%2Bkdd4G%2BSm7iqLjFbi2pUCzYLcoZ%2Buzt5H%2B1MyMPgQ8IosbyASHpcC94pyUj5%2BvoK%2FtugYFZUkZYx3QCIjNBjbyeI%2B1trF3aKl3Q8Po2%2FBh%2BGbbVAKTsFQoA%2FB%2BscvosQ8aiAu2k2oYY463HEX807K8%2FKfgiQmpk%2BJkrTLbklIJpcMFFqftuZg%3D%3D" alt="instagram-icon" class="community-icon">
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img src="https://storage.googleapis.com/vue-course-api.appspot.com/reirei/1725193225623.png?GoogleAccessId=firebase-adminsdk-zzty7%40vue-course-api.iam.gserviceaccount.com&Expires=1742169600&Signature=SaarVV82Nzp41uF0gTADC2zwIDTV2iV4882wZIx3StPDApMoT56pLBlp%2BOEk%2FSCUmAgVuZNvf3k9ZsdmAnu1Z%2Bs4CMrMGl3ORQfpacxSa73b0x6s3Ihn3grw4uV0PF9KzmWubni0OW8aQE%2F7nEkj0e5KcqqRu%2BHtMGKUTUzChSW1caMP93wxBVyufOyfrvINMnqIFBphXvlkCGHuOLn%2FXqo2for9mGuAOXOzQsBqIBAKX3wmCku5B8p2mobvNvYee2Qvxj4plubVLFSnEm9B4HvAMfT%2BZEf08osTvUmYtSFOVIZ6TBJDzCUfoAu4SWjF9112tLkL2B%2FPGZXaspVqPw%3D%3D" alt="twitter-icon" class="community-icon">
+          </a>
+        </li>
+      </ul>
+      <span class="d-lg-block d-none text-light text-end my-7">隱私權政策</span>
+      <p class="statement text-center mt-5">Copyright © 2024 road driving serivices.</p>
+    </div>
+  </footer>
 </template>
 
 <script>
