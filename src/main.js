fab2a29ebfacf679805f4f982419637cf324164b
kawa <>
2024-09-22 12:39:33 +0800

    載入VeeValidate

--- src/main.js
+++ src/main.js
@@ -4,16 +4,33 @@ import 'bootstrap-icons/font/bootstrap-icons';
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-// import { Swiper, SwiperSlide } from 'swiper/vue';
+
+import {
+  Field, Form, ErrorMessage, defineRule, configure,
+} from 'vee-validate';
+import { localize, setLocale } from '@vee-validate/i18n';
+import { all as rules } from '@vee-validate/rules';
+import zhTW from '@vee-validate/i18n/dist/locale/zh_TW.json';
 
 import App from './App.vue';
 import router from './router';
-// import VueAwesomeSwiper from 'vue-aweome-swiper';
+
+Object.keys(rules).forEach((rule) => {
+  defineRule(rule, rules[rule]);
+});
+
+configure({
+  generateMessage: localize({ zh_TW: zhTW }),
+  validateOnInput: true,
+});
+setLocale('zh_TW');
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-// app.use(VueAwesomeSwiper);
 
+app.component('VField', Field);
+app.component('VForm', Form);
+app.component('ErrorMessage', ErrorMessage);
 app.mount('#app');
