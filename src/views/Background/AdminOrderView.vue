bf11e43e801f03ea82b0fc8803275011b499635c
kawa <>
2024-10-15 17:27:34 +0800

    新增編輯、刪除api

--- src/views/Background/AdminOrderView.vue
+++ src/views/Background/AdminOrderView.vue
@@ -37,19 +37,28 @@
                 </li>
               </ul>
             </td>
-            <td>{{ order.is_paid ? '已完課' : '未完課'  }}</td>
+            <td>
+              <div class="form-check form-switch">
+                  <input class="form-check-input" v-model="order.is_paid" type="checkbox"
+                  role="switch"
+                    :id="`paidSwitch${order.id}`" @change="addOrderStates(order)">
+                  <label class="form-check-label" :for="`paidSwitch${order.id}`">
+                    {{ order.is_paid ? '已完課' : '未完課'  }}
+                  </label>
+                </div>
+              </td>
             <td>
               <a href="#">看詳細</a>
             </td>
             <td>
               <div class="btn-group">
                 <button type="button" class="btn btn-outline-primary btn-sm"
-                data-bs-toggle="modal" data-bs-target="#exampleModal"
+                data-bs-toggle="modal" data-bs-target="#orderModal"
                 >
                   編輯
                 </button>
                 <button type="button" class="btn btn-outline-danger btn-sm"
-                >
+                @click="delOrder">
                   刪除
                 </button>
               </div>
@@ -66,6 +75,48 @@
           </li>
         </ul>
       </nav>
+      <div class="modal fade" id="orderModal"
+        tabindex="-1" aria-labelledby="orderModal" aria-hidden="true">
+        <div class="modal-dialog modal-xl">
+              <div class="modal-content border-0">
+                <div class="modal-header bg-dark text-white">
+                  <h5 id="productModalLabel" class="modal-title">
+                    <span >新增產品</span>
+                    <span >編輯產品</span>
+                  </h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal"
+                  aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  <div class="row">
+                    <div class="col-3">
+                      <div class="mb-3">
+                        <label for="imageUrl" class="form-label">主要圖片</label>
+                        <input id="imageUrl" type="text"
+                        class="form-control"
+                          placeholder="請輸入圖片連結">
+                        </div>
+                        <div class="mb-3">
+                          <label for="file" class="form-label">上傳圖片
+                            <i class="fas fa-spinner fa-spin"></i>
+                          </label>
+                          <input type="file" class="form-control mb-4" id="file" ref="fileInput"
+                          @change="uploadPhotos"/>
+                        </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
+                    取消
+                  </button>
+                  <button type="button" class="btn btn-primary">
+                    確認
+                  </button>
+                </div>
+              </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -82,6 +133,7 @@ export default {
     return {
       orderData: {},
       isloading: false,
+      isNew: false,
     };
   },
   methods: {
@@ -91,6 +143,67 @@ export default {
         .then((res) => {
           this.orderData = res.data.orders;
           this.isloading = false;
+        })
+        .catch((err) => {
+          this.isloading = false;
+          Swal.fire({
+            toast: true,
+            position: 'center',
+            showConfirmButton: false,
+            timer: 1500,
+            icon: 'error',
+            title: err.response.data.message,
+          });
+        });
+    },
+    addOrderStates(item) {
+      this.isloading = true;
+      const paid = {
+        is_paid: item.is_paid,
+      };
+      axios.put(`${VITE_URL}/api/${VITE_PATH}/admin/order/${item.id}`, { data: paid })
+        .then(() => {
+          this.isNew = false;
+          this.isloading = false;
+          this.$refs.orderModal.modalClose();
+          this.getOrders();
+          Swal.fire({
+            toast: true,
+            position: 'center',
+            showConfirmButton: false,
+            timer: 1500,
+            icon: 'success',
+            title: '編輯成功',
+          });
+        })
+        .catch((err) => {
+          this.isloading = false;
+          Swal.fire({
+            toast: true,
+            position: 'center',
+            showConfirmButton: false,
+            timer: 1500,
+            icon: 'error',
+            title: err.response.data.message,
+          });
+        });
+    },
+    delOrder() {
+      this.isNew = true;
+      this.isloading = true;
+      axios.delete(`${VITE_URL}/api/${VITE_PATH}/admin/order/${this.orderData.id}`)
+        .then(() => {
+          this.isNew = false;
+          this.isloading = false;
+          this.getOrders();
+          Swal.fire({
+            toast: true,
+            position: 'center',
+            showConfirmButton: false,
+            timer: 1500,
+            icon: 'success',
+            title: '刪除成功',
+          });
         })
         .catch((err) => {
           this.isloading = false;
